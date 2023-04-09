aabf004ea33eb4cbdc6754198567b0d34ec1fa4d
Luis Juarez <luisisthejuarez@gmail.com>
2023-04-09 19:54:18 -0400

    updated header/footer and main page for closets

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,22 +11,13 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>RoomGPT</title>
+        <title>ClosetGPT</title>
       </Head>
 
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
-        <a
-          href="https://vercel.fyi/roomGPT"
-          target="_blank"
-          rel="noreferrer"
-          className="border border-gray-700 rounded-lg py-2 px-4 text-gray-400 text-sm mb-5 transition duration-300 ease-in-out hover:text-gray-300"
-        >
-          Clone and deploy your own with{" "}
-          <span className="text-blue-600">Vercel</span>
-        </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
-          Generating dream rooms{" "}
+          Generating dream closets{" "}
           <span className="relative whitespace-nowrap text-blue-600">
             <SquigglyLines />
             <span className="relative">using AI</span>
@@ -34,14 +25,14 @@ const Home: NextPage = () => {
           for everyone.
         </h1>
         <h2 className="mx-auto mt-12 max-w-xl text-lg sm:text-gray-400  text-gray-500 leading-7">
-          Take a picture of your room and see how your room looks in different
-          themes. 100% free – remodel your room today.
+          Take a picture of your closet and see how your closet looks in different
+          themes.
         </h2>
         <Link
           className="bg-blue-600 rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-blue-500 transition"
           href="/dream"
         >
-          Generate your dream room
+          Generate your dream closet
         </Link>
         <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
@@ -50,7 +41,7 @@ const Home: NextPage = () => {
                 <h3 className="mb-1 font-medium text-lg">Original Room</h3>
                 <Image
                   alt="Original photo of a room with roomGPT.io"
-                  src="/original-pic.jpg"
+                  src="/originalCloset.jpeg"
                   className="w-full object-cover h-96 rounded-2xl"
                   width={400}
                   height={400}
@@ -62,7 +53,7 @@ const Home: NextPage = () => {
                   alt="Generated photo of a room with roomGPT.io"
                   width={400}
                   height={400}
-                  src="/generated-pic-2.jpg"
+                  src="/genCloset.jpeg"
                   className="w-full object-cover h-96 rounded-2xl sm:mt-0 mt-2"
                 />
               </div>
@@ -70,7 +61,6 @@ const Home: NextPage = () => {
           </div>
         </div>
       </main>
-      <Testimonials />
       <Footer />
     </div>
   );
