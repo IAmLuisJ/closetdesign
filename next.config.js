b55e64619d2df3e59ddde2e16f4235e2187c403c
Luis Juarez <luisisthejuarez@gmail.com>
2023-10-28 04:26:42 -0400

    added remotePatterns config

--- next.config.js
+++ next.config.js
@@ -3,5 +3,11 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["upcdn.io", "replicate.delivery"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pbxt.replicate.delivery',
+      },
+    ],
   },
 };
